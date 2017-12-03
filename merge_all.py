e18a5078cb21c8310cfd9e25a297894fb2c30952
QQ565253906 <565253906@qq.com>
2017-12-03 22:19:18 +0800

    Merge the all sheets in one excel
    Not only the 1st sheet in each excel

--- merge_all.py
+++ merge_all.py
@@ -10,22 +10,25 @@ for f in arr_f:
 	print ('Begin load '+str(loop+1)+'/'+str(length)+' : '+f)
 	wb_B = openpyxl.load_workbook(f)
 	wb_B_sheets = wb_B.get_sheet_names()
-	S1 = wb_B_sheets[0]
-	# print("\t"+"S1:"+S1) 
-	if loop== 0:
-		r_a = 0
-	else:
-		r_a = sheet_a.max_row
-	c_a = sheet_a.max_column
-	sheet_b = wb_B.get_sheet_by_name(S1)
-	r_b = sheet_b.max_row
-	c_b = sheet_b.max_column
-
-	# print('\t\t'+'r_a:'+str(r_a)+'  r_b:'+str(r_b)+'  c_a:'+str(c_a)+'  c_b:'+str(c_b))
-	for row in range(1+(loop!=0), r_b+1 ):
-		for col in range(1, c_b+1):
-			sheet_a[get_column_letter(col)+str(r_a+row)].value=sheet_b[get_column_letter(col)+str(row)].value
-	loop += 1 
+	for S1 in wb_B_sheets:
+		# print("\t"+"S1:"+S1) 
+		if loop== 0:
+			r_a = 0
+		else:
+			r_a = sheet_a.max_row
+		c_a = sheet_a.max_column
+		sheet_b = wb_B.get_sheet_by_name(S1)
+		r_b = sheet_b.max_row
+		c_b = sheet_b.max_column
+		
+		# print('\t\t'+'r_a:'+str(r_a)+'  c_a:'+str(c_a)+'  r_b:'+str(r_b)+'  c_b:'+str(c_b))
+		if r_b == 1 and c_b == 1:
+			continue	
+			
+		for row in range(1+(loop!=0), r_b+1 ):
+			for col in range(1, c_b+1):
+				sheet_a[get_column_letter(col)+str(r_a+row)].value=sheet_b[get_column_letter(col)+str(row)].value
+		loop += 1 
 	print ('End   load '+str(loop)+'/'+str(length)+' : '+f)
 wb_A.save(output)
 print('Done..Will finish after 5s')
